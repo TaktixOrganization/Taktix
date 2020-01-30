bf1dbb40cfdba69eb12d8c9587fc0fa0873eb8de
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2020-01-30 14:46:42 +0900

    Updated .gitignore

---
+++ Build/build_osx.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Compiling SFML from sources
+
+cd Submodules/SFML
+git checkout 2.5.x
+mkdir -p Build
+cd Build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+sudo make install
+
+cd ../../..
+
+# Compiling TGUI from sources
+
+cd Submodules/TGUI
+git checkout 0.8
+mkdir -p Build
+cd Build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+sudo make install
+
+cd ../../..
+
+# Compiling Taktix
+
+cd Build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make all
+
+# Moving libraries
+
+mkdir -p Application/Binaries/
+mv Application/Taktix Application/Binaries/.
+mkdir -p Application/Submodules
+cp ../Submodules/SFML/Build/lib/* Application/Submodules/.
+cp ../Submodules/TGUI/Build/lib/* Application/Submodules/.
+chmod u+x run.sh
+cp run.sh Application/.
+
+mkdir -p Tests/Binaries/
+mv Tests/Taktix_Tests Tests/Binaries/.
+mkdir -p Tests/Submodules
+cp ../Submodules/SFML/Build/lib/* Tests/Submodules/.
+cp ../Submodules/TGUI/Build/lib/* Tests/Submodules/.
+
+# Cleaning deployement files
+
+rm Makefile
+rm *.cmake
+rm -rf CMakeFiles
+rm CMake*
+
+rm Application/Makefile
+rm Application/*.cmake
+rm -rf Application/CMakeFiles
+
+rm Tests/Makefile
+rm Tests/*.cmake
+rm -rf Tests/CMakeFiles
