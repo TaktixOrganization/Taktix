a902d584985c9d93697bc9988cf85510ee96f9ff
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2020-01-31 17:17:26 +0900

    Updated Windows CI

--- .ci/ci_windows.bat
+++ .ci/ci_windows.bat
@@ -15,7 +15,6 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (
     SET CMAKE_IMAGE_NAME=Visual Studio 16 2019
     SET VS_DATE=vs2019
-    SET PROGRAM_FILES=Program Files (x86)
 )
 
 IF "%platform%"=="Win32" (
@@ -26,6 +25,10 @@ IF "%platform%"=="Win32" (
 IF "%platform%"=="x64" (
     SET CMAKE_GENERATOR_NAME=%CMAKE_IMAGE_NAME% Win64
     SET PROGRAM_FILES=Program Files
+)
+
+IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (
+    SET PROGRAM_FILES=Program Files (x86)
 )
 
 ECHO %image%
@@ -39,7 +42,11 @@ CD SFML
 git checkout 2.5.x
 MD build
 CD build
-cmake -G "%CMAKE_GENERATOR_NAME%" -DCMAKE_BUILD_TYPE=Release ..
+IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (
+    cmake -G "%CMAKE_GENERATOR_NAME%" -A "%platform%" -DCMAKE_BUILD_TYPE=Release ..
+) ELSE (
+    cmake -G "%CMAKE_GENERATOR_NAME%" -DCMAKE_BUILD_TYPE=Release ..
+)
 cmake --build . --config "Release"
 cmake --build . --target INSTALL --config "Release"
 CD ../..
@@ -50,13 +57,21 @@ CD TGUI
 git checkout 0.8
 MD build
 CD build
-cmake -G "%CMAKE_GENERATOR_NAME%" -DCMAKE_BUILD_TYPE=Release -DSFML_ROOT="C:/%PROGRAM_FILES%/SFML/" ..
+IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (
+    cmake -G "%CMAKE_GENERATOR_NAME%" -A "%platform%" -DCMAKE_BUILD_TYPE=Release -DSFML_ROOT="C:/%PROGRAM_FILES%/SFML/" ..
+) ELSE (
+    cmake -G "%CMAKE_GENERATOR_NAME%" -DCMAKE_BUILD_TYPE=Release -DSFML_ROOT="C:/%PROGRAM_FILES%/SFML/" ..
+)
 cmake --build . --config "Release"
 cmake --build . --target INSTALL --config "Release"
 CD ../../..
 
 CD Build
-cmake -G "%CMAKE_GENERATOR_NAME%" -DCMAKE_BUILD_TYPE=Release -DSFML_ROOT="C:/%PROGRAM_FILES%/SFML/" -DTGUI_ROOT="C:/%PROGRAM_FILES%/TGUI/" ..
+IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (
+    cmake -G "%CMAKE_GENERATOR_NAME%" -A "%platform%" -DCMAKE_BUILD_TYPE=Release -DSFML_ROOT="C:/%PROGRAM_FILES%/SFML/" -DTGUI_ROOT="C:/%PROGRAM_FILES%/TGUI/" ..
+) ELSE (
+    cmake -G "%CMAKE_GENERATOR_NAME%" -DCMAKE_BUILD_TYPE=Release -DSFML_ROOT="C:/%PROGRAM_FILES%/SFML/" -DTGUI_ROOT="C:/%PROGRAM_FILES%/TGUI/" ..
+)
 cmake --build . --config "Release"
 
 XCOPY ..\Submodules\SFML\build\lib\Release\*.dll Application\. /s /e /y
